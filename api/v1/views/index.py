1ad79fba2ad5177c1ff277da51b303c93f7825fb
Abraham Ayamigah <abkc070@gmail.com>
2024-03-31 16:24:43 -0700

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 # the status of the api route
@@ -10,3 +11,18 @@ from flask import jsonify
 def get_status():
     """Get status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Gets stats of objects by type"""
+    classes = {
+        'amenities': 'Amenity',
+        'cities': 'City',
+        'places': 'Place',
+        'reviews': 'Review',
+        'states': 'State',
+        'users': 'User'
+    }
+    stats = {key: storage.count(value) for key,value in classes}
+    return jsonify(stats)
